afb45cf92e20ae4b48f7fed74fca0636d9a547fe
Gaye0523 <maya_1819@hotmail.com>
2020-07-31 13:41:46 -0500

    Agregar Imagenes

---
+++ src/data.js
@@ -0,0 +1,19 @@
+
+export const filtroDato = (dato, rol) => {
+  const arrayDato = Object.values(dato)
+  const resulDato = arrayDato.filter(objetRol => {
+    const etiquetasObjets = objetRol.tags
+    // console.log(rol)
+    const valuesDato = etiquetasObjets.includes(rol)
+    return valuesDato
+  })
+  return resulDato
+}
+
+export const filtroNombre = (datos, nameSearch) => {
+  let usuario = nameSearch.value.toLowerCase();
+  const arrayData = Object.values(datos);
+  const personajes = Array.from(arrayData);
+  const personajeEncontrado = personajes.find(personaje => personaje.name.toLowerCase() === usuario)
+  return personajeEncontrado;
+}
